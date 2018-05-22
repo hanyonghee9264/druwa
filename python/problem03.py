13a4fbdb7153833da2186d4b6874efd316f6c3af
hanyonghee <hanyonghee9264@gmail.com>
2018-05-22 12:39:08 +0900

    함수를 이용한 거듭제곱과 곱셈문제

--- python/problem03.py
+++ python/problem03.py
@@ -1,3 +1,12 @@
 """
 한 개 또는 두 개의 숫자 인자를 전달받아, 하나가 오면 제곱, 두개를 받으면 두 수의 곱을 반환해주는 함수를 정의하고 사용해본다.
 """
+
+def math(num1,num2):
+     ...:     square = num1 ** num2
+     ...:     multiple = num1 * num2
+     ...:     return square, multiple
+
+# math(2,3)
+# (8, 6)
+
